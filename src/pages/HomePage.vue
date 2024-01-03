66ac6c9812acdab489171b3523a5ae21c16d8f9c
Sergen Aslan <sergenaslan09@gmail.com>
2024-01-04 00:14:34 +0300

    kelimnn ezberlenen kategorye taşınabilmesi yapıldı

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -11,7 +11,7 @@
             <wordMeaning v-for="word in wordMeaningData" :data="word"></wordMeaning>
         </div> 
         <div class="button-container">
-            <button @click="setSmileCategory" :class="{active : showWord.btnSmile}">
+            <button @click="setSmileCategory(showWord)" :class="{active : showWord.btnSmile}">
                 <svg xmlns="http://www.w3.org/2000/svg" height="35" width="35" viewBox="0 0 512 512"><path fill="white" d="M464 256A208 208 0 1 0 48 256a208 208 0 1 0 416 0zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm177.6 62.1C192.8 334.5 218.8 352 256 352s63.2-17.5 78.4-33.9c9-9.7 24.2-10.4 33.9-1.4s10.4 24.2 1.4 33.9c-22 23.8-60 49.4-113.6 49.4s-91.7-25.5-113.6-49.4c-9-9.7-8.4-24.9 1.4-33.9s24.9-8.4 33.9 1.4zM144.4 208a32 32 0 1 1 64 0 32 32 0 1 1 -64 0zm192-32a32 32 0 1 1 0 64 32 32 0 1 1 0-64z"/></svg> 
             </button>
             <button @click="setThinkCategory(showWord)" :class="{active : showWord.btnThink}">
@@ -78,8 +78,33 @@ export default {
       };
     },
     methods:{
+        /* kategoriyi değiştir */
+        changeWordCategory: function(targetArr){
+            if(this.showingCategory.length > 1){
+                    if(this.activeWordNumber === this.showingCategory.length-1){
+                        this.activeWordNumber--;
+                        this.showingCategory = this.showingCategory.filter(word =>{
+                            if(word.kelime !== this.showingCategory[this.activeWordNumber+1].kelime){
+                                return word;
+                            }else{
+                                targetArr.push(word);
+                            }
+                        });
+
+                    }else{
+                        this.showingCategory = this.showingCategory.filter(word =>{
+                            if(word.kelime !== this.showingCategory[this.activeWordNumber].kelime){
+                                return word;
+                            }else{
+                                targetArr.push(word);
+                            }
+                        });
+                    }
+                }
+        },
+
         /* yukarıdaki butonlar */
-        setSmileCategory: async function(){
+        setSmileCategory: async function(word){
             if(!this.showWord.btnSmile){
                 console.log('setSmileCategory');
                 this.changeWordCategory(this.ezberlenenKelime);
@@ -150,29 +175,6 @@ export default {
             }
         },
 
-        changeWordCategory: function(targetArr){
-            if(this.showingCategory.length > 1){
-                    if(this.activeWordNumber === this.showingCategory.length-1){
-                        this.activeWordNumber--;
-                        this.showingCategory = this.showingCategory.filter(word =>{
-                            if(word.kelime !== this.showingCategory[this.activeWordNumber+1].kelime){
-                                return word;
-                            }else{
-                                targetArr.push(word);
-                            }
-                        });
-
-                    }else{
-                        this.showingCategory = this.showingCategory.filter(word =>{
-                            if(word.kelime !== this.showingCategory[this.activeWordNumber].kelime){
-                                return word;
-                            }else{
-                                targetArr.push(word);
-                            }
-                        });
-                    }
-                }
-        },
         prevWord : function(){
             if(this.activeWordNumber  > 0  ){
                 this.activeWordNumber--
