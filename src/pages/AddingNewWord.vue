5e655851babe6b3c59362c405246c60d6f6132b1
Sergen Aslan <sergenaslan09@gmail.com>
2024-01-20 23:42:36 +0300

    yeni kelime ekleme sayfası kodlandı

--- src/pages/AddingNewWord.vue
+++ src/pages/AddingNewWord.vue
@@ -1,21 +1,23 @@
 <template>
     <div class="form">
-        <p>Kelime Ekleme Sayfası</p>
-        <input type="text" placeholder="Kelime" v-model="wordData.kelime">
-        <input type="text" placeholder="Okunuşu">
-        <input type="text" placeholder="İsim Kelimesi">
-        <input type="text" placeholder="Sıfat Kelimesi">
-        <input type="text" placeholder="Zarf  Kelimesi">
-        <input type="text" placeholder="Fiil  Kelimesi">
+        <h2 class="pageTitle">Kelime Ekleme Sayfası</h2>
+        <input type="text" placeholder="Kelime" :class="{error:kelimeInValid}" v-model="wordData.kelime" :disabled="formDisabled">
+        <p class="inputErrorText">Lütfen Kelime Giriniz...</p>
+        <input type="text" placeholder="Okunuşu" :class="{error:okunusInValid}" v-model="wordData.okunus" :disabled="formDisabled">
+        <p class="inputErrorText">Lütfen Kelimenin Okunuşunu Giriniz..</p>
+        <input type="text" :class="{error:wordNotMeaningError}" placeholder="İsim Kelimesi" v-model="wordData.isim" :disabled="formDisabled">
+        <input type="text" :class="{error:wordNotMeaningError}" placeholder="Sıfat Kelimesi" v-model="wordData.sifat" :disabled="formDisabled">
+        <input type="text" :class="{error:wordNotMeaningError}" placeholder="Zarf  Kelimesi" v-model="wordData.zarf" :disabled="formDisabled">
+        <input type="text" :class="{error:wordNotMeaningError}" placeholder="Fiil  Kelimesi" v-model="wordData.fiil" :disabled="formDisabled">
         <img v-if="uploadedImageShow" class="uploaded-image" :src="wordData.resimYol">
         <label v-if="!uploadedImageShow" class="file-upload">
           <input type="file" ref="fileInput" @change="handleUpload" :disabled="fileUploadInputDisabled">
           {{fileUploadInputButtonText}}
         </label>
-        <button @click="addWordToFirebase">Ekle</button>
+        <button class="saveButton" @click="addWordToFirebase" :disabled="formDisabled || sendButtonDisabled"> {{ sendButtonText }} </button>
+        <p class="formWarningText" v-if="formErrorText !== null">{{formErrorText}}</p>
     </div>
 </template>
-
 <script>
 import {auth ,createUser,db,getDocs,getDoc,signInWithEmailAndPassword,
   collection ,addDoc,deleteDoc, doc, updateDoc,signOut, storage, ref, uploadBytes,getDownloadURL
@@ -24,39 +26,107 @@ export default {
     name: 'AddingNewWord',
     data() {
         return{
+            kelimeInValid: false,
+            okunusInValid: false,
             uploadedImageShow: false,
             fileUploadInputDisabled: false,
+            wordNotMeaningError : false,
             fileUploadInputButtonText : 'Dosya Seçiniz',
+            sendButtonText: 'Kaydet',
             wordData : {
+                kelime : null,
+                okunus: null,
+                zarf : null,
+                sifat : null,
+                isim : null,
+                fiil : null,
                 btnAngry : true,
                 btnSmile : false,
                 btnThink : false,
-                fiil : null,
-                isim : null,
-                kelime : null,
-                knowNumber : '',
-                okunus: null,
-                resimYol: 'https://firebasestorage.googleapis.com/v0/b/kelime-ezber-e0e0e.appspot.com/o/kelime-ezber%2Fsoap.jpg?alt=media&token=2906724d-a09f-49d6-a661-7f7eb63a89c9',
-                sifat : null,
-                zarf : null
+                knowNumber : '0',
+                resimYol: null,
             },
-            dbName : 'kelime-ezber'
+            dbName : 'kelime-ezber',
+            formErrorText:null,
+            formDisabled: false,
+            sendButtonDisabled:false,
         }
     },
     methods: {
+        isDataValid: function(value){
+            if(value !== null && value.trim() !== ''){
+                return true;
+            }
+            return false;
+        },
+        formReset: function(){
+           this.wordData = {
+                kelime : null,
+                okunus: null,
+                zarf : null,
+                sifat : null,
+                isim : null,
+                fiil : null,
+                btnAngry : true,
+                btnSmile : false,
+                btnThink : false,
+                knowNumber : '0',
+                resimYol: null,
+            };
+            this.uploadedImageShow = false;
+            this.sendButtonText = 'Kaydet';
+        },
+        formValidationReset: function(){
+            this.kelimeInValid = false;
+            this.okunusInValid = false;
+            this.wordNotMeaningError = false;
+        },
+        formValidation: function(){
+            if(!this.isDataValid(this.wordData.kelime)){
+                this.kelimeInValid = true;
+                return false;
+            }
+            if(!this.isDataValid(this.wordData.okunus)){
+                this.okunusInValid = true;
+                return false;
+            }
+            if( !this.isDataValid(this.wordData.fiil) &&
+                !this.isDataValid(this.wordData.isim) &&
+                !this.isDataValid(this.wordData.sifat) &&
+                !this.isDataValid(this.wordData.zarf)
+              ){
+                this.formErrorText = "Lütfen Kelimenin Türkçe Anlamını Yazınız";
+                this.wordNotMeaningError = true;
+                return false;
+            }
+            if(!this.isDataValid(this.wordData.resimYol)){
+                this.formErrorText = "Lütfen Bir Resim Yükleyiniz...";
+                return false;
+            }
+            return true;
+        },
         addWordToFirebase :async function(){
-            console.log('naber gelmedi senden bi haber');
+            console.log('asdfajjj');
+            this.formValidationReset();
+            if( !this.formValidation() ){
+                return
+            }
             console.log(this.wordData);
+            this.formErrorText = null;
             try {
+                this.formDisabled = true;
+                this.sendButtonText = 'Yükleniyor...';
                 let res = await addDoc(collection(db,this.dbName), this.wordData);
-                    console.log(res.id);
-                    
+                console.log(res.id);
+                this.formDisabled = false;
+                this.formReset();
+                this.formErrorText = "Resim Başarılı Bir Şekilde Yüklendi.";
+                setTimeout(() => {
+                    this.formErrorText = null;
+                }, 2000);
                 } catch (error) {
                     console.log(error);
             }
-        },
-        deneme: function(){
-            console.log('lan lun bekle');
         },
         handleUpload: async function(){
             const file = this.$refs.fileInput.files[0]; // Seçilen dosyayı al
@@ -71,6 +141,7 @@ export default {
 
             try {
                 // Dosyayı yükleme işlemi
+                this.sendButtonDisabled = true;
                 const snapshot = await uploadBytes(storageRef, file);
                 console.log('Dosya yükleme işlemi başarılı:', snapshot);
                 console.log(snapshot)
@@ -82,15 +153,61 @@ export default {
                 this.fileUploadInputDisabled = false;
                this.fileUploadInputButtonText = 'Dosya Seçiniz';
                this.uploadedImageShow = true;
+               this.sendButtonDisabled = false;
             } catch (error) {
                 console.error('Dosya yükleme işlemi başarısız:', error);
             }
         } 
+    },
+    watch : {
+        'wordData.kelime' : function(){
+            if(this.kelimeInValid){
+                this.kelimeInValid = false;
+            }
+        },
+        'wordData.okunus' : function(){
+            if(this.okunusInValid){
+                this.okunusInValid = false;
+            }
+        },
+        'wordData.fiil' : function(){
+            if(this.wordNotMeaningError){
+                this.wordNotMeaningError = false;
+            }
+        },
+        'wordData.isim' : function(){
+            if(this.wordNotMeaningError){
+                this.wordNotMeaningError = false;
+            }
+        },
+        'wordData.sifat' : function(){
+            if(this.wordNotMeaningError){
+                this.wordNotMeaningError = false;
+            }
+        },
+        'wordData.zarf' : function(){
+            if(this.wordNotMeaningError){
+                this.wordNotMeaningError = false;
+            }
+        }
     }
 }
 
 </script>
 <style scoped>
+h2{
+    font-family: 'Shadows Into Light', cursive;
+    color:white;
+    margin-top: 20px;
+    margin-bottom: 40px;
+    position: relative;
+    top: 10px;
+    font-family: 'Shadows Into Light', cursive;
+    color: white;
+    text-align: center;
+    font-size: 22px;
+    letter-spacing: 4px;
+}
 .form{
     display:flex;
     flex-direction: column;
@@ -102,8 +219,15 @@ export default {
     padding:10px;
     border: none;
 }
-.form button{
+.form .saveButton{
     border:none;
+    padding:5px 10px;
+    border-radius:5px;
+    margin-top:15px;
+    cursor: pointer;
+}
+.form .saveButton:disabled{
+    background-color: #e2e2e2;
 }
 .file-upload{
     background-color: #b290e4;
@@ -120,5 +244,28 @@ export default {
     width: 200px;
     height: 150px;
     object-fit: contain;
+}
+
+.form .formWarningText{
+    color:white;
+    margin-top:20px;
+}
+.inputErrorText{
+    font-size: 14px;
+    color:red;
+    margin-top: -5px;
+    margin-bottom: 5px;
+    height: 0;
+    overflow: hidden;
+    transition: .3s ease;
+}
+.form input.error{
+    border: 1px solid red;
+}
+.form input.error +  .inputErrorText{
+    height: 17px;
+}
+.form input:disabled{
+    background-color: #e2e2e2;    
 }
 </style>
