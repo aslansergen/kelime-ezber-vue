db8f944a5f86f4e1a523f39a902faf0fcd1705d3
Sergen Aslan <sergenaslan09@gmail.com>
2024-05-26 17:22:19 +0300

    kullancıı kaydetme özelliği eklendi

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore, getDocs, collection, addDoc,deleteDoc,getDoc,doc,updateDoc,} from 'firebase/firestore';
-import { getAuth, createUserWithEmailAndPassword,signInWithEmailAndPassword,signOut } from 'firebase/auth';
+import { getAuth, createUserWithEmailAndPassword,signInWithEmailAndPassword,signOut,updateProfile } from 'firebase/auth';
 
 import { getStorage, ref, uploadBytes,getDownloadURL,deleteObject } from 'firebase/storage';
 
@@ -27,6 +27,6 @@ const firebaseConfig = {
   
   export { db, auth, createUser, getDocs, collection, addDoc, deleteDoc, doc,
      updateDoc,getDoc, signInWithEmailAndPassword,signOut,
-     storage, ref, uploadBytes,getDownloadURL, storageRef, deleteObject
+     storage, ref, uploadBytes,getDownloadURL, storageRef, deleteObject, updateProfile
     };
 
