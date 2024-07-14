9df81092daab68cfd084f69ca2ccf3c52a5a7c71
Sergen Aslan <sergenaslan09@gmail.com>
2024-07-14 20:30:21 +0300

    link component created

---
+++ src/components/UserLinkComponent.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="userMenu">
+    <span>
+      
+    </span>
+    {{status }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserLinkComponent',
+  data() {
+    return {
+      status: 'Hello Vue.js!',
+    };
+  },
+}
+</script>
+
+<style scoped>
+.userMenu {
+  position: absolute;
+  width: 50px;
+  height: 50px;
+  border: 1px solid red;
+}
+</style>
--- src/pages/Deneme.vue
+++ src/pages/Deneme.vue
@@ -1,3 +1,18 @@
 <template>
-    <p>şlasdfaşlksdjf</p>
-</template>
+    <div>
+      <p>kaşsdlkfajsdf</p>
+      <UserLinkComponent />
+    </div>
+  </template>
+  
+  <script>
+  import UserLinkComponent from '../components/UserLinkComponent.vue';
+  
+  export default {
+    name: 'Deneme', // Ana bileşenin adını belirlemek iyi bir uygulamadır
+    components: {
+      UserLinkComponent
+    }
+  }
+  </script>
+  
