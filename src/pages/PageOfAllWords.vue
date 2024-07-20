e03eb8947b7558ba08c6183811e3aa429ff1a6ba
Sergen Aslan <sergenaslan09@gmail.com>
2024-07-20 18:09:06 +0300

    kelime tablosu düzenlendi

--- src/pages/PageOfAllWords.vue
+++ src/pages/PageOfAllWords.vue
@@ -1,23 +1,38 @@
 <template>
-    <UserLinkComponent />
-    <table v-if="totalData.length > 0">
-      <thead>
-        <tr>
-          <th>Kelime</th>
-          <th>Okunus</th>
-          <th>Resim Linki</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="word in totalData">
-          <td> {{word.kelime}} </td>
-          <td> {{word.okunus}} </td>
-          <td> <a :href="word.resimYol" target="_blank"> Aç </a> </td>
-        </tr>
-      </tbody>
-    </table>
-    <div>
-      <p> {{ totalData.length }} </p>
+    <div class="container">
+      <UserLinkComponent />
+      <div class="table" v-if="totalData.length > 0">
+        <div class="thead">
+          <div class="theadTh">
+            Kelime
+          </div>
+          <div class="theadTh">
+            Okunus
+          </div>
+          <div class="theadTh">
+            Anlamı
+          </div>
+          <div class="theadTh">
+            Resim Linki
+          </div>
+        </div>
+        <div class="tbody">
+          <div class="tbodyTr" v-for="word in totalData">
+            <div class="tbodyTd">
+              {{word.kelime}} 
+            </div>
+            <div class="tbodyTd">
+              {{word.okunus}} 
+            </div>
+            <div class="tbodyTd">
+              {{word.okunus}} 
+            </div>
+            <div class="tbodyTd">
+              <a :href="word.resimYol" target="_blank"> Aç </a> 
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </template>
   
@@ -27,7 +42,7 @@ import {dataCheck} from '../utili/utility'
 import { mapGetters, mapActions } from 'vuex';
 
 
-  export default {
+export default {
     name: 'PageOfAllWords',
     computed: {
       ...mapGetters({
@@ -47,6 +62,42 @@ import { mapGetters, mapActions } from 'vuex';
         this.getWordData;
       }
     }
+}
+</script>
+<style scoped>
+  .container{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: 1px solid white;
+    min-height: 100vh;
   }
-  </script>
+  .table{
+    border: 1px solid red;
+    background-color: white;
+    padding: 10px;
+    border-radius: 8px;
+  }
+  .thead{
+    border: 1px solid black;
+    display: flex;
+    flex-direction: row;
+  }
+  .theadTh{
+    padding: 7px;
+    width: 100px;
+    border: 1px solid red;
+    font-weight: bold;
+  }
+  .tbodyTr{
+    display: flex;
+    flex-direction: row;
+    border: 1px solid black;
+  }
+  .tbodyTd{
+    width: 100px;
+    padding: 7px 0;
+    border: 1px solid red;
+  }
+</style>
   
